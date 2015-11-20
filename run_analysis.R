cf21848a30e057dae8ed73f4d86b1a1059115cb9
Gunnar Fornes <gunnarfornes@gmail.com>
2015-11-20 20:34:38 +0100

    Add stuff for step 4 &5

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,38 @@
-library(data.table)
-library(plyr)
+require(data.table)
+require(dplyr)
 
-# 1. Merges the training and the test sets to create one data set.
-
-feature_names <- as.character(read.table('dataset/features.txt')$V2)
-
+# Merges the training and the test sets to create a new data set
 X_train <- fread('dataset/train/X_train.txt')
 X_test <- fread('dataset/test/X_test.txt')
 X_merged <- rbind(X_train, X_test)
+
+# Appropriately labels the data set with descriptive variable names.
+feature_names <- as.character(read.table('dataset/features.txt')$V2)
 X_merged <- setNames(X_merged, feature_names)
 
-print(names(X_merged))
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+X_merged <- select(X_merged, matches("mean|std"))
 
+# Add activity id and name to the new data set
 y_train <- fread('dataset/train/y_train.txt')
 y_test <- fread('dataset/test/y_test.txt')
 y_merged <- rbind(y_train, y_test)
+activity_labels <- read.table('dataset/activity_labels.txt', col.names=c("id", "activity"))
+X_merged <- mutate(X_merged, activityId = y_merged)
+X_merged <- merge(X_merged, activity_labels, by.x="activityId", by.y="id")
 
-print(head(y_merged))
-
+# Add subject id information to the new data set
 subject_train <- fread('dataset/train/subject_train.txt')
 subject_test <- fread('dataset/test/subject_test.txt')
 subject_merged <- rbind(subject_train, subject_test)
+X_merged <- mutate(X_merged, subjectId=subject_merged$V1)
 
-# print(head(X_merged))
-# print(head(y_merged))
+# Write to file
+write.table(X_merged, file="output_step4.txt")
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names.
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+newSet <- X_merged %>%
+  group_by(activity, subjectId) %>%
+  summarize_each(funs(mean))
 
-print("DONE")
+write.table(newSet, file="output_step5.txt", row.name=FALSE)
